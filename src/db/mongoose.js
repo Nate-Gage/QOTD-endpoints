8646a5c96487132fe86e5e0c041d230920e8e2f7
Nate <nate@Nates-MacBook-Air.local>
2019-11-06 18:52:31 -0700

    Added dynamic building of URL

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose')
+const nconf = require('nconf')
 
-mongoose.connect('mongodb://127.0.0.1:27017/quotes', {
+// Read in keys and secrets. Using nconf we can set secrets via keys.json file.
+nconf.argv().env().file('keys.json')
+
+const user = nconf.get('mongoUser')
+const pass = nconf.get('mongoPass')
+const host = nconf.get('mongoHost')
+const port = nconf.get('mongoPort')
+
+let uri = 'mongodb:${user}:${pass}@${host}:${port}'
+
+//and then replace your 127.0.0.1 connection value with the uri property
+
+mongoose.connect(uri + '/quotes', {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true,
